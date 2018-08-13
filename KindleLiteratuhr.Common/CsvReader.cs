cafea3faff302004e96bbdf5700935232c0bff6b
timokuper <timo.kuper@gmail.com>
2018-08-13 10:51:28 +0200

    Schönere Anführungszeichen im Text.

--- KindleLiteratuhr.Common/CsvReader.cs
+++ KindleLiteratuhr.Common/CsvReader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,14 +14,39 @@ namespace KindleLiteratuhr.Common
                         let contents = line.Split('|')
                         select new TimeData
                         {
+
                             Time = contents[0],
                             TimeInText = contents[1],
-                            Text = contents[2]
-                                .Replace("\"\"\"", "\"")    // """ -> "
-                                .Replace("\"\"", "\"")      // "" -> "
-                                .Replace("''", "\""),       // '' -> "
-                            Book = contents[3],
-                            Author = contents[4]
+                            Text = contents[2].Modify(t =>
+                           {
+                               string text = t.Trim();
+                               var sb = new StringBuilder(text);
+
+                               sb.Replace("\"\"\"", "\"")    // """ -> "
+                               .Replace("\"\"", "\"")      // "" -> "
+                               .Replace("''", "\"");        // '' -> "
+
+                               int pos = -1;
+                               var quotes = new List<int>();
+                               while ((pos = sb.ToString().IndexOf('"', pos + 1)) > -1)
+                               {
+                                   quotes.Add(pos);
+                               }
+
+                               if (quotes.Count % 2 == 0)
+                               {
+                                   for (int i = 0; i < quotes.Count; i += 2)
+                                   {
+                                       sb.Remove(quotes[i], 1);
+                                       sb.Insert(quotes[i], "“");
+                                       sb.Remove(quotes[i + 1], 1);
+                                       sb.Insert(quotes[i + 1], "”");
+                                   }
+                               }
+                               return sb.ToString();
+                           }),
+                            Book = contents[3].Trim(),
+                            Author = contents[4].Trim()
                         };
 
             return times;
---
+++ KindleLiteratuhr.Common/ModifyExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KindleLiteratuhr.Common
+{
+    public static class ModifyExtension
+    {
+        public static T Modify<T>(this T obj, Func<T, T> action)
+        {
+            return action(obj);
+        }
+    }
+}
